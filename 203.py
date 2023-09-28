e45a874b2f61e85bf011a879c48aa23db06e54b0
SecureGeekette <141087625+SecureGeekette@users.noreply.github.com>
2023-09-27 23:08:14 -0700

    Solved LC 203.py - Remove Linked List Elements

---
+++ 203.py
@@ -0,0 +1,38 @@
+203. Remove Linked List Elements
+
+Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+
+Input: head = [], val = 1
+Output: []
+
+Input: head = [7,7,7,7], val = 7
+Output: []
+
+Solution:
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        
+        dummy = ListNode()
+        dummy.next = head
+
+        current = dummy
+
+        while current is not None:
+            if current.next is not None and current.next.val == val:
+                    current.next = current.next.next
+            else:
+                current = current.next
+
+        return dummy.next
+
+  Learnings:
+- Learnt we could add a dummy node for similar questions. Also was returning head which was a mistake incase it gets deleted in the code, we should've been returning dummy.next
