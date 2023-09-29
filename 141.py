f7d23f95bf5ab1346030e3cd0314cd8fcee225ef
SecureGeekette <141087625+SecureGeekette@users.noreply.github.com>
2023-09-29 08:51:11 -0700

    Solved LC 141.py - Linked List Cycle

--- 141.py
+++ 141.py
@@ -42,3 +42,27 @@ class Solution:
                 return True
 
         return False
+
+
+So the way to solve this with O(1) time complexity is using the Floyd's Tortoise and Hare algorithm
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+
+        return False
+
+   
