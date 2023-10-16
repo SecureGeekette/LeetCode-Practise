586ea0775019b4f6dfc79bf60376e2c4c81c4b56
SecureGeekette <141087625+SecureGeekette@users.noreply.github.com>
2023-10-15 20:46:36 -0700

    Solved LC 3.py - Longest Substring without repeating characters

--- 3.py
+++ 3.py
@@ -19,3 +19,26 @@ Notice that the answer must be a substring, "pwke" is a subsequence and not a su
 Constraints:
 0 <= s.length <= 5 * 104
 s consists of English letters, digits, symbols and spaces.
+
+Solution:
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        s = list(s)
+        hash_map = {}
+        len_string = 0
+        max_len = 0
+        start = 0
+
+        for i in range(len(s)):
+
+            if s[i] in hash_map:
+                start = max(start, hash_map[s[i]]+1)
+
+            len_string = i - start + 1
+            max_len = max(max_len, len_string)
+            hash_map[s[i]] = i
+
+        return max_len
+        
