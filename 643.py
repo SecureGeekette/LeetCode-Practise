2f9e7d73f0529469337c44e9ed3e99c3d97e5f9f
SecureGeekette <141087625+SecureGeekette@users.noreply.github.com>
2023-10-15 19:37:44 -0700

    Solved LC 643.py - Maximum Average Subarray 1

---
+++ 643.py
@@ -0,0 +1,39 @@
+643. Maximum Average Subarray I
+
+You are given an integer array nums consisting of n elements, and an integer k.
+
+Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+
+Input: nums = [1,12,-5,-6,50,3], k = 4
+Output: 12.75000
+Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+
+Input: nums = [5], k = 1
+Output: 5.00000
+ 
+
+Constraints:
+n == nums.length
+1 <= k <= n <= 105
+-104 <= nums[i] <= 104
+
+
+Solution:
+
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+
+        cur_average = sum(nums[:k])
+        max_average = cur_average
+
+        if len(nums) <= k:
+            return sum(nums)/k
+        else:
+            for i in range(k,len(nums)):
+                cur_average = cur_average - nums[i-k] + nums[i]
+                if cur_average > max_average:
+                    max_average = cur_average
+
+        return max_average/k
+
+  Typical Sliding Window problem
