4ba6a8479dac1892f507e6816fef988167c57e0d
SecureGeekette <141087625+SecureGeekette@users.noreply.github.com>
2023-10-24 11:27:46 -0700

    Update Log Parser.py with syslog parser script

--- Log Parser.py
+++ Log Parser.py
@@ -41,7 +41,7 @@ Examples:
 ^[A-Z][a-z]*$: Matches strings that start with an uppercase letter followed by zero or more lowercase letters.
 
 
-  Assuming we have Apache web server logs, we have to write a parser which details the IP Address, HTTP method, HTTP protocol, HTTP status code, response size, timestamp etc.
+Assuming we have Apache web server logs, we have to write a parser which details the IP Address, HTTP method, HTTP protocol, HTTP status code, response size, timestamp etc.
 
 127.0.0.1 - - [01/Nov/2023:10:30:42 +0000] "GET /index.html HTTP/1.1" 200 512
 
@@ -74,6 +74,54 @@ print("Status Codes: ")
 print(status_code)
 
 
+
+Let's draft a parser for syslogs (syslog is a standard logging protocol used by network devices, servers and applications to send log messages to a central logging server called a syslog server).
+
+Examples:
+
+Simple syslog message: Oct 24 14:32:15 hostname This is a simple syslog message.
+Syslog Message with Facility and Severity Levels: Oct 24 14:45:22 localhost.localdomain cron[1234]: (root) CMD ( /usr/local/bin/backup.sh )
+Syslog Message with Timestamp, Hostname, and Program Name: 2023-10-24T16:12:30.001Z firewall.example.com firewall: Packet dropped: Suspicious incoming connection from 203.0.113.42
+Syslog Message with Custom Information: Oct 24 17:15:28 appserver application: [INFO] User 'alice' successfully created a new account.
+
+Here we'll follow a different regex approach
+
+#This is not working code, just me trying out stuff (ChatGPT suggestions used)
+
+import re
+
+sample_syslog_1 = "Oct 24 14:45:22 localhost.localdomain cron[1234]: (root) CMD ( /usr/local/bin/backup.sh )"
+sample_syslog_2 = "2023-10-24T16:12:30.001Z firewall.example.com firewall: Packet dropped: Suspicious incoming connection from 203.0.113.42"
+sample_syslog_3 = "Oct 24 17:15:28 appserver application: [INFO] User 'alice' successfully created a new account."
+
+#In inconsistent log formats like these, we have to assume one log format and do fitting
+
+#Instead of doing individual term matching like below
+#ip_pattern = r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b"
+##message = r"\b:\w+\b"
+
+#ips = re.findall(ip_pattern, sample_syslog_2)
+#print("IPs: ")
+#print(ips)
+
+#We'll use a named capturing group like this <name>
+
+syslog_pattern = r'(?P<timestamp>.*?)\s(?P<hostname>.*?)\s(?P<program>.*?)\[(?P<process_id>\d+)\]:(?P<message>.*)'
+match = re.match(syslog_pattern, sample_syslog_2)
+
+if match:
+    groups = match.groupdict()
+    # Extracting individual components from the matched groups
+    timestamp = groups['timestamp']
+    hostname = groups['hostname']
+    program = groups['program']
+    process_id = groups['process_id']
+    message = groups['message']
+
+    print(f'timestamp: {timestamp}, hostname: {hostname}, process_id: {process_id}, message: {message.strip()}')
+else:
+    # Return None if the syslog message doesn't match the pattern
+    print("None")
 
 
 
