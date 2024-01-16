e8295c287ba8d551832809abd5536e0473399433
SecureGeekette <141087625+SecureGeekette@users.noreply.github.com>
2024-01-15 20:56:03 -0800

    Create pdf_metadata_extraction.py

---
+++ pdf_metadata_extraction.py
@@ -0,0 +1,12 @@
+from PyPDF2 import PdfReader
+
+path = "eBook - How to Build a Career in AI.pdf"
+
+with open(path,'rb') as f:
+  pdf = PdfReader(f)
+  info = pdf.metadata
+  print(info)
+
+
+Example output:
+{'/CreationDate': "D:20221213160800-05'00'", '/Creator': 'Adobe InDesign 18.0 (Macintosh)', '/ModDate': "D:20221213160804-05'00'", '/Producer': 'Adobe PDF Library 17.0', '/Trapped': '/False'}
