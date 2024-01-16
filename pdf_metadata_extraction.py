66146695ebbb08b0787d3d7a2fab81b129157c66
SecureGeekette <141087625+SecureGeekette@users.noreply.github.com>
2024-01-15 21:00:38 -0800

    Update pdf_metadata_extraction.py

--- pdf_metadata_extraction.py
+++ pdf_metadata_extraction.py
@@ -10,3 +10,13 @@ with open(path,'rb') as f:
 
 Example output:
 {'/CreationDate': "D:20221213160800-05'00'", '/Creator': 'Adobe InDesign 18.0 (Macintosh)', '/ModDate': "D:20221213160804-05'00'", '/Producer': 'Adobe PDF Library 17.0', '/Trapped': '/False'}
+
+Same thing can also be done via another library:
+
+import pikepdf
+
+path = "eBook - How to Build a Career in AI.pdf"
+pdf = pikepdf.Pdf.open(path)
+docinfo = pdf.docinfo
+print(docinfo)
+  
