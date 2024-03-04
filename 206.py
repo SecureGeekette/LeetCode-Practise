19e18f96919a498f26f759d235786c28a203667a
SecureGeekette <garudasuma@gmail.com>
2024-03-04 13:41:26 -0800

    Solved LC 206.py - Reverse Linked List

--- 206.py
+++ 206.py
@@ -11,38 +11,6 @@ Output: [2,1]
 Input: head = []
 Output: []
 
-
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-
-        current = head
-        nodes = []
-
-        while current:
-            nodes.append(current)
-            current = current.next
-
-        dummy = ListNode(0)
-
-        if len(nodes) > 0:
-            dummy.next = nodes[-1]
-            current = dummy.next
-
-            for i in range(len(nodes)-1, 0, -1):
-                current.next = nodes[i-1]
-                current = current.next
-            current.next = None
-
-        return dummy.next
-
-
-The problem with this code is that it takes 0(n) memory to store all the list nodes. We can use the two pointer approach to do this in O(1)
-
 def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
         if not head:
