a723a34eea2b7c047c61a77029f8f6637d178370
SecureGeekette <garudasuma@gmail.com>
2024-03-05 12:28:57 -0800

    Solved 3Sum 15.py with optimized 2 pointer approach

--- 15.py
+++ 15.py
@@ -41,4 +41,28 @@ class Solution:
 
         return answer
 
-        
+
+Optimized solution with 2 pointer approach
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+
+        nums.sort()
+        answer = set()
+
+        for i in range(len(nums)-2):
+            start = i+1
+            end = len(nums)-1
+            while start < end:
+                total = nums[i] + nums[start] + nums[end]
+                if total < 0:
+                    start += 1
+                elif total > 0:
+                    end -= 1
+                else:
+                    answer.add((nums[i], nums[start], nums[end]))
+                    start += 1
+                    end -= 1
+
+        return answer
+
