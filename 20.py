518a9bcf890fa4a827eefd0e49802308040b45f3
SecureGeekette <garudasuma@gmail.com>
2024-03-08 14:49:58 -0800

    Solved LC 20.py - Valid Parentheses

--- 20.py
+++ 20.py
@@ -37,5 +37,25 @@ class Solution:
             return False
 
 
+Solution when I tried it the second time:
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        map_bracket = {')':'(', ']': '[', '}':'{'}
+        stack_bracket = []
+
+        for bracket in s:
+            if bracket == '(' or bracket == '[' or bracket == '{':
+                stack_bracket.append(bracket)
+            else:
+                if not stack_bracket or stack_bracket.pop() != map_bracket[bracket]:
+                    return False 
+        
+        if not stack_bracket:
+            return True
+        else:
+            return False
+
+
 Mistakes:
 - Range error because I didn't check if stack exists but was checking top of stack
