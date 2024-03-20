91ef611acdefdead2acd23c71795ee79dcf0421c
SecureGeekette <garudasuma@gmail.com>
2024-03-19 17:29:34 -0700

    Solved LC 55.py - Jump Game - Greedy Approach

--- 55.py
+++ 55.py
@@ -35,3 +35,21 @@ class Solution:
 
         return False
         
+
+Greedy solution O(n)
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+
+        goal = len(nums)-1
+
+        for i in range(len(nums)-2, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+                
+        if goal == 0:
+            return True
+        else:
+            return False
+
+
+       
