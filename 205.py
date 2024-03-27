c565247bce556db258d6e93e3046fcfac81af2d7
SecureGeekette <garudasuma@gmail.com>
2024-03-27 11:59:13 -0700

    Solve LC 205.py - Optimized Isomorphic Strings

--- 205.py
+++ 205.py
@@ -40,3 +40,38 @@ class Solution:
                             return False
         
         return True
+
+
+Optimized solution with Zipped Set
+
+why using zip ?
+=> zip function would pair the first item of first iterator (i.e s here) to the first item of second iterator (i.e t here). set() would remove duplicate items from the zipped tuple. It is like the first item of first iterator mapped to the first item of second iterator as it would in case of a hashtable or dictionary.
+Understand using examples:
+
+# when strings ae isomorphic:
+s = "egg"
+t = "add"
+
+zipped_set = {('e', 'a'), ('g', 'd')}
+# now comparing their lengths when duplicacies are removed
+return len(zipped_set) == len(set(s)) == len(set(d))
+# return 2 == 2 == 2 -> True
+# when strings are not isomorphic:
+s = "egk"
+t = "add"
+
+zipped_set = {('e', 'a'), ('g', 'd'), ('k', 'd')}
+# now comparing their lengths when duplicacies are removed
+return len(zipped_set) == len(set(s)) == len(set(d))
+
+# return 3 == 3 == 2 -> False
+
+    def isIsomorphic(self, s: str, t: str) -> bool:
+
+        zipped_set = set(zip(s, t))
+        
+        if len(zipped_set) == len(set(s)) == len(set(t)):
+            return True
+        else:
+            return False
+
